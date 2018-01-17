d552ea6cacd622028b4a11ced32f45a7157ca90c
Yilin <yilingu@Yilins-MBP.fios-router.home>
2018-01-16 21:23:23 -0500

    final project updates 01

---
+++ final-project/03/Silk Cakes/js/main.js
@@ -0,0 +1,13 @@
+var isActive = false;
+
+$('.js-menu').on('click', function() {
+	if (isActive) {
+		$(this).removeClass('active');
+		$('body').removeClass('menu-open');
+	} else {
+		$(this).addClass('active');
+		$('body').addClass('menu-open');
+	}
+
+	isActive = !isActive;
+});
